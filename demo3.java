952d12ca0b653cff65452533a374e884dfa2885a
safihabanu <safihabca@gmail.com>
2023-03-03 09:48:43 +0530

    applied stash to get the old code

--- demo3.java
+++ demo3.java
@@ -1,5 +1,12 @@
 class A{
+<<<<<<< Updated upstream
 	//stash to save
 	int a ,b ;
 	int c= a* b;
+=======
+	//stash apply
+	int a , b;
+	int c= a+b;
+	system.out.println(c);
+>>>>>>> Stashed changes
 }
